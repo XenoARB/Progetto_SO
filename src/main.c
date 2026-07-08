ff8954b40197faa6718c94252bd99e2207011b60
Matteo <matteo.gargaro00@gmail.com>
2026-07-08 16:44:25 +0200

    Pulizia main debugger

--- src/main.c
+++ src/main.c
@@ -7,35 +7,5 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    const char *path = argv[1];
-    size_t size = 40960; 
-    fs_format(path, size);
-
-    MmappedFile mf;
-    mf_open(&mf, path);
-    mf_map(&mf);
-
-    uint32_t allocated[16];
-    int count = 0;
-
-    uint32_t idx;
-    while ((idx = alloc_block(&mf)) != BLOCK_NONE) {
-        allocated[count++] = idx;
-    }
-    printf("blocchi allocati: %d\n", count);
-
-    uint32_t extra = alloc_block(&mf);
-    printf("alloc_block a disco pieno ritorna: %u\n", extra);
-
-    for (int i = 0; i < count; i++) {
-        free_block(&mf, allocated[i]);
-    }
-
-    Superblock *sb = (Superblock *) mf.mem;
-    print_superblock(&mf);
-    print_chain(&mf, sb->head);
-
-    mf_close(&mf);
-
-    return 0;
+    
 }
