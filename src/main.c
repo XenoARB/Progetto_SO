b36a8ac6e78d83579dad7f1bc4179b3e0a815211
Matteo <matteo.gargaro00@gmail.com>
2026-07-09 15:12:13 +0200

    Loop principale del main, lettura e tokenizzazione degli input

--- src/main.c
+++ src/main.c
@@ -1,8 +1,34 @@
-#include "mmapped_file.h"
-#include "fs_format.h"
-#include"fs_debug.h"
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE 256
+#define MAX_ARGS 16
 
 
-int main(int argc, char **argv) {
+int main() {
+    char line[MAX_LINE];
 
+    while(1){
+        printf("> ");
+        char *input=fgets(line, sizeof(line), stdin);
+        
+        if(input==NULL) break;
+
+        line[strcspn(line, "\n")] = '\0';
+
+        char *args[MAX_ARGS];
+        int argc=0;
+
+        char *token = strtok(line, " ");
+        while(token != NULL && argc<MAX_ARGS){
+            args[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        if(argc==0) continue;
+
+        printf("comando: %s (argc=%d)\n", args[0], argc);
+    }
+
+    return 0;
 }
