c7d5a1a8bdeb60043d5a9e2d96f3c3270429758e
Matteo <matteo.gargaro00@gmail.com>
2026-07-09 08:30:08 +0200

    Struct DirEntry e funzione di ricerca find()

--- src/fs_types.h
+++ src/fs_types.h
@@ -18,4 +18,19 @@ typedef struct{
     uint32_t next;
 }Blockheader;
 
+#define ENTRY_FREE 0
+#define ENTRY_FILE 1
+#define ENTRY_DIR 2
+#define NAME_MAX_LEN 28
+
+typedef struct{
+    char name[NAME_MAX_LEN];
+    uint8_t type;
+    uint32_t first_block;
+    uint32_t size;
+}DirEntry;
+
+
+#define ENTRY_BLOCK ((BLOCK_SIZE - sizeof(Blockheader))) / sizeof(DirEntry)
+
 
